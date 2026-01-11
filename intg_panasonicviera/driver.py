6fc28f023800d5323af1ae2aca9fadeffcc1f024
Meir Miyara <3089864+mase1981@users.noreply.github.com>
2026-01-11 10:52:28 -0600

    fix

--- intg_panasonicviera/driver.py
+++ intg_panasonicviera/driver.py
@@ -6,7 +6,6 @@ Panasonic Viera TV driver for Unfolded Circle Remote.
 """
 
 import logging
-from typing import Any
 from ucapi import Entity, EntityTypes
 from ucapi.media_player import Attributes as MediaAttributes
 from ucapi_framework import BaseIntegrationDriver
@@ -40,26 +39,6 @@ class PanasonicVieraDriver(BaseIntegrationDriver[PanasonicVieraDevice, Panasonic
         _LOG.info("Created %d entities for %s", len(entities), device_config.name)
         return entities
 
-    def map_device_state(
-        self,
-        entity: Entity,
-        device: PanasonicVieraDevice,
-        event_data: Any = None
-    ) -> dict[str, Any]:
-        if entity.entity_type == EntityTypes.MEDIA_PLAYER:
-            if event_data:
-                return event_data
-
-            return {
-                MediaAttributes.STATE: "ON" if device.power else "OFF",
-                MediaAttributes.VOLUME: device.volume,
-                MediaAttributes.MUTED: device.muted,
-                MediaAttributes.SOURCE: device.current_source,
-                MediaAttributes.SOURCE_LIST: device.source_list,
-            }
-
-        return {}
-
     async def refresh_entity_state(self, entity_id: str) -> None:
         _LOG.debug("[%s] Refreshing entity state", entity_id)
 
@@ -89,6 +68,7 @@ class PanasonicVieraDriver(BaseIntegrationDriver[PanasonicVieraDevice, Panasonic
                 source_list = await device.get_sources()
 
             if source_list:
-                attributes = self.map_device_state(configured_entity, device)
-                self.api.configured_entities.update_attributes(entity_id, attributes)
+                self.api.configured_entities.update_attributes(
+                    entity_id, {MediaAttributes.SOURCE_LIST: source_list}
+                )
                 _LOG.info("[%s] Updated entity state with %d sources", entity_id, len(source_list))
